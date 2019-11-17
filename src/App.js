cc10244785292b44d3d25e2c57dee55f0b63a5a8
k-2tha-brimm <kev.brimm@gmail.com>
2019-11-17 01:54:07 -0800

    add y histo. begin setting up lifecycle method for update so that charts dont overlap

--- src/App.js
+++ src/App.js
@@ -124,6 +124,77 @@ class App extends React.Component {
       }
   }
 
+  populateYHisto() {
+
+    if (this.state.yVal) {
+
+      let data = this.state.data.map(datum => {
+        return datum[this.state.yVal];
+      });
+
+      let formatCount = d3.format(",.0f");
+      let margin = {
+              top: 10,
+              right: 30,
+              bottom: 30,
+              left: 30
+            }
+      let width = 400;
+      let height = 200;
+      
+      let container = d3.select(".y-axis-histogram")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      
+      let g = container.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      let x = d3.scaleLinear()
+        .domain(d3.extent(data))
+        .rangeRound([0, width])
+
+      let histogram = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(5));
+
+      let bins = histogram(data);
+
+      let y = d3.scaleLinear()
+        .domain([0, d3.max(bins, function(d) {
+          return d.length;
+        })])
+        .range([height, 0]);
+
+      let bar = g.selectAll(".bar")
+        .data(bins)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) {
+          return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+        });
+
+        bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 2)
+        .attr("height", function(d) {
+          return height - y(d.length);
+        });
+    
+      bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+          return formatCount(d.length);
+        });
+    
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    }
+}
+
   handleXChange(e) {
     this.setState({ xVal: e.currentTarget.value });
     this.populateXHisto();
@@ -131,6 +202,7 @@ class App extends React.Component {
 
   handleYChange(e) {
     this.setState({ yVal: e.currentTarget.value });
+    this.populateYHisto();
   }
 
   render() {
@@ -168,6 +240,7 @@ class App extends React.Component {
               {renderedValues}
             </select>
             <p>You have selected {this.state.yVal || `nothing, yet!`}</p>
+            <div className="y-axis-histogram"></div>
           </div>
         </div>
       </div>
