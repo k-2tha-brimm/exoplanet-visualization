35cff41cb968da665b80a0b1aa93d188cdb3e174
k-2tha-brimm <kev.brimm@gmail.com>
2019-11-17 01:49:13 -0800

    add XHisto, but need to add bins

--- src/App.js
+++ src/App.js
@@ -53,8 +53,80 @@ class App extends React.Component {
     })
   }
 
+  populateXHisto() {
+
+      if (this.state.xVal) {
+
+        let data = this.state.data.map(datum => {
+          return datum[this.state.xVal];
+        });
+
+        let formatCount = d3.format(",.0f");
+        let margin = {
+                top: 10,
+                right: 30,
+                bottom: 30,
+                left: 30
+              }
+        let width = 400;
+        let height = 200;
+        
+        let container = d3.select(".x-axis-histogram")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+        
+        let g = container.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        let x = d3.scaleLinear()
+          .domain(d3.extent(data))
+          .rangeRound([0, width])
+
+        let histogram = d3.histogram()
+          .domain(x.domain())
+          .thresholds(x.ticks(5));
+
+        let bins = histogram(data);
+
+        let y = d3.scaleLinear()
+          .domain([0, d3.max(bins, function(d) {
+            return d.length;
+          })])
+          .range([height, 0]);
+
+        let bar = g.selectAll(".bar")
+          .data(bins)
+          .enter().append("g")
+          .attr("class", "bar")
+          .attr("transform", function(d) {
+            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+          });
+
+          bar.append("rect")
+          .attr("x", 1)
+          .attr("width", x(bins[0].x1) - x(bins[0].x0) - 2)
+          .attr("height", function(d) {
+            return height - y(d.length);
+          });
+      
+        bar.append("text")
+          .attr("dy", ".75em")
+          .attr("y", 6)
+          .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+          .attr("text-anchor", "middle")
+          .text(function(d) {
+            return formatCount(d.length);
+          });
+      
+        g.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+      }
+  }
+
   handleXChange(e) {
     this.setState({ xVal: e.currentTarget.value });
+    this.populateXHisto();
   }
 
   handleYChange(e) {
@@ -80,13 +152,18 @@ class App extends React.Component {
         </header>
 
         <div className="histogram-container">
-          <div className="x-axis-selector-container">
+          <div className="selector-container">
+            <label>X-Axis</label>
+            <br />
             <select onChange={(e) => this.handleXChange(e)}>
               {renderedValues}
             </select>
             <p>You have selected {this.state.xVal || `nothing, yet!`}</p>
+            <div className="x-axis-histogram"></div>
           </div>
-          <div className="y-axis-selector-container">
+          <div className="selector-container">
+            <label>Y-Axis</label>
+            <br />
             <select onChange={(e) => this.handleYChange(e)}>
               {renderedValues}
             </select>
